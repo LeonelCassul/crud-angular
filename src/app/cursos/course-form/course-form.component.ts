1d3e73bac714597ec538dffdb54c4a9e30382988
Leonel Cassul <leonelsalmos@gmail.com>
2023-07-28 23:16:06 +0100

    Cancel button

--- src/app/cursos/course-form/course-form.component.ts
+++ src/app/cursos/course-form/course-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { CategoriaPipe } from '../../shared/pipes/categoria.pipe';
 import { CursosService } from '../services/cursos.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -13,7 +14,7 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
   constructor(private formBuilder: FormBuilder, private service: CursosService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar, private location: Location) {
     this.form = this.formBuilder.group({
       nome: [null],
       categoria: [null]
@@ -28,15 +29,21 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit(){
     this.service.save(this.form.value).subscribe
-    (result => console.log(result), error => this.onError());
+    (result => this.onSucess(), error => this.onError());
     ;
 
   }
 
   onCancel(){
+    this.location.back();
 
   }
- private onError(){
+
+  private onSucess(){
+    this.snackBar.open('Curso salvo com Sucesso!','', {duration: 5000});
+    this.onCancel();
+  }
+  private onError(){
   this.snackBar.open('Erro ao salvar o curso.','', {duration: 5000});
 }
 }
